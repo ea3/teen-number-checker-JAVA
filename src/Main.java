1365dd6bc5a80ad529c76a6d5971713c1d0682d4
Emilio Araos <ea3dev@gmail.com>
2019-10-30 09:23:59 -0600

    setting up repository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String [] args){
+
+       boolean result=  TeenNumberChecker.hasTeen(9, 99, 19);
+       System.out.println(result);
+
+       boolean resultTwo= TeenNumberChecker.isTeen(19);
+        System.out.println(resultTwo);
+
+    }
+}
